bfa8259a29c8617bc790eb16797dcf0fa75d14fc
alok tiwari <aloksstiwari@gmail.com>
2020-05-14 20:50:26 +0530

    Setup to read keys from settings.globalcontext

--- config-airtable.js
+++ config-airtable.js
@@ -1,9 +1,14 @@
 const Airtable = require("airtable");
 
-module.exports = function(RED) {
-  const register = function(config) {
+module.exports = function (RED) {
+  const register = function (config) {
     RED.nodes.createNode(this, config);
     let credentials = this.credentials;
+    var airtableConfig = RED.settings.functionGlobalContext.get('airtable');
+    if (airtableConfig && airtableConfig[config.name]) {
+      console.log('Using credentional from Settings.globalcontext');
+      credentials = airtableConfig[config.name];
+    }
     if (!credentials || !credentials.app || !credentials.key) return;
     this.app = new Airtable({ apiKey: credentials.key }).base(credentials.app);
   };
